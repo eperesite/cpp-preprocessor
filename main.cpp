9cbc51c1d4a7770074ca9c5379fd980f31cfa4cb
Margarita <127439584+eperesite@users.noreply.github.com>
2025-08-28 23:00:17 +0300

    1 коммит

--- main.cpp
+++ main.cpp
@@ -14,13 +14,88 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+bool ProcessFile(ifstream& input, const path& file_path, ostream& output, const vector<path>& include_directories);
+
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+    ifstream input(in_file);
+    if (!input.is_open()) {
+        return false;
+    }
+
+    ofstream output(out_file);
+    if (!output.is_open()) {
+        return false;
+    }
+
+    bool success = ProcessFile(input, in_file, output, include_directories);
+    return success;
+}
+
+bool ProcessFile(ifstream& input, const path& file_path, ostream& output, const vector<path>& include_directories) {
+    static const regex include_quoted(R"raw(\s*#\s*include\s*"([^"]*)"\s*)raw");
+    static const regex include_angled(R"raw(\s*#\s*include\s*<([^>]*)>\s*)raw");
+
+    string line;
+    int line_number = 0;
+    while (getline(input, line)) {
+        ++line_number;
+        smatch match;
+        if (regex_match(line, match, include_quoted)) {
+            path include_path = match[1].str();
+            path current_dir = file_path.parent_path();
+            path full_path = current_dir / include_path;
+            
+            ifstream include_file(full_path);
+            if (!include_file.is_open()) {
+                bool found = false;
+                for (const auto& dir : include_directories) {
+                    full_path = dir / include_path;
+                    include_file.open(full_path);
+                    if (include_file.is_open()) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    cout << "unknown include file " << include_path.string() << " at file " << file_path.string() << " at line " << line_number << endl;
+                    return false;
+                }
+            }
+            if (!ProcessFile(include_file, full_path, output, include_directories)) {
+                return false;
+            }
+        } else if (regex_match(line, match, include_angled)) {
+            path include_path = match[1].str();
+            bool found = false;
+            path full_path;
+            ifstream include_file;
+
+            for (const auto& dir : include_directories) {
+                full_path = dir / include_path;
+                include_file.open(full_path);
+                if (include_file.is_open()) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                cout << "unknown include file " << include_path.string() << " at file " << file_path.string() << " at line " << line_number << endl;
+                return false;
+            }
+
+            if (!ProcessFile(include_file, full_path, output, include_directories)) {
+                return false;
+            }
+        } else {
+            output << line << "\n";
+        }
+    }
+    return true;
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
-
-    // конструируем string по двум итераторам
     return {(istreambuf_iterator<char>(stream)), istreambuf_iterator<char>()};
 }
 
